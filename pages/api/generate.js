2a5875774dc6449c98f471504941d7a8e70bf10c
Shawn Pradeep <shawnprdp@gmail.com>
2023-08-01 20:45:02 -0400

    init commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,18 +9,39 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const name = req.body.name || "";
+  if (name.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: "Please enter a valid name",
+      },
+    });
+    return;
+  }
+
+  const color = req.body.color || "";
+  if (color.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid color",
+      },
+    });
+    return;
+  }
+
+  const season = req.body.season || "";
+  if (season.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid season",
+      },
     });
     return;
   }
@@ -28,11 +49,11 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(name, color, season),
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,22 +62,13 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(name, color, season) {
+  return `Generated a target ad to ${name}, advertising an outfit that he would enjoy knowing that his favorite color is ${color} and the current season is ${season}`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
+  const [colorInput, setColorInput] = useState("");
+  const [seasonInput, setSeasonInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,17 +16,24 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({
+          name: nameInput,
+          color: colorInput,
+          season: seasonInput,
+        }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
       setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -34,22 +43,36 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Clothes Micromarketing Demo</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="./smile.svg" className={styles.icon} />
+        <h3>Enter preferences</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="name"
+            placeholder="Enter your name"
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input
+            type="text"
+            name="color"
+            placeholder="Enter your favorite color"
+            value={colorInput}
+            onChange={(e) => setColorInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="season"
+            placeholder="Enter the current season"
+            value={seasonInput}
+            onChange={(e) => setSeasonInput(e.target.value)}
+          />
+          <input type="submit" value="Generate targeted suggestion" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
